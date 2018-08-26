e6b445c3674ad0765c87e42cfa2fedff732b607c
haramaki <>
2018-08-26 20:31:13 +0900

    エラーが出ても自動コンパイルが止まらないようにする処理

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var packageImporter = require('node-sass-package-importer');
+
 gulp.task('sass', function() {
-    gulp.src('*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
+    gulp.src('./sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            importer: packageImporter({
+                extensions: ['.scss', '.css']
+            })
         }))
+        .on('error', function(err) {
+            console.log(err.message);
+          })
+        .pipe(gulp.dest('./css/'))
 });
 gulp.task('default', ['sass'], function() {
     gulp.watch('./sass/*.scss', ['sass']);
